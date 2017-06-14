932510aaf38612ef4cb7ce89ae633ef1c9f95c22
Matt Lim <mlim@caltech.edu>
2017-06-14 16:03:25 -0700

    Add github source link

--- src/HtmlGen.hs
+++ src/HtmlGen.hs
@@ -13,6 +13,8 @@ render xs = html_ $ do
   head_ $ do
     title_ "Happy page"
     link_ [rel_ "stylesheet", type_ "text/css", href_ "css/screen.css"]
-  body_ [id_ "mybody", onload_ "ran_col()", class_ "container", width_ "100%", style_ "font-family:Bookman Old Style;"]
-    $ script_ [src_ "color-script.js"] "" <>
-      (term "center" $ mconcat $ fmap h1_ xs)
+  body_ [id_ "mybody", onload_ "ran_col()", width_ "100%", style_ "font-family:Bookman Old Style;"]
+    $ script_ [src_ "color-script.js"] ""
+      <> (term "center" [class_ "container"] $ mconcat $ fmap h1_ xs)
+      <> (div_ [style_ "position: absolute; bottom: 20px; right: 30px; text-align:left"]
+         $ a_ [href_ "https://github.com/arcticmatt/happy-site"] "view source")
