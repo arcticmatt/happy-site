d864b81a87c1a5f681c5755016a621389e427850
Matt Lim <mlim@caltech.edu>
2017-06-14 18:04:59 -0700

    Change page title

--- src/HtmlGen.hs
+++ src/HtmlGen.hs
@@ -11,7 +11,7 @@ import Data.Functor.Identity (Identity)
 render :: [HtmlT Identity ()] -> Html ()
 render xs = html_ $ do
   head_ $ do
-    title_ "Happy page"
+    title_ "Happy Things"
     link_ [rel_ "stylesheet", type_ "text/css", href_ "css/screen.css"]
   body_ [id_ "mybody", onload_ "ran_col()", width_ "100%", style_ "font-family:Bookman Old Style;"]
     $ script_ [src_ "color-script.js"] ""
